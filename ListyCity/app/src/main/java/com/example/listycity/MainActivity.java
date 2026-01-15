324c26fdd54c332f4d7c27f4691957c5eda47f2d
nzapisocki <nzapisoc@ualberta.ca>
2026-01-14 22:15:20 -0700

    Completed In Lab Portion

---
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityList = findViewById(R.id.city_list);
+
+        String []cities = {"Edmonton", "Calgary", "Toronto", "Winnipeg", "London"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        // button = findViewById(R.id.button_add);
+        // button.setOnClickListener(new View.OnClickListener())
+
+    }
+}
