efef8873500360b053a20689e4fb71c50ad4cbbf
nzapisocki <nzapisoc@ualberta.ca>
2026-01-15 20:21:39 -0700

    Implemented Buttons

--- ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.listycity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
 
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    int selectedPosition = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,8 +41,52 @@ public class MainActivity extends AppCompatActivity {
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
 
-        // button = findViewById(R.id.button_add);
-        // button.setOnClickListener(new View.OnClickListener())
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedPosition = position;
+        });
+
+        Button addButton = findViewById(R.id.add_button);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final EditText input = new EditText(MainActivity.this);
+                input.setHint("Enter city name");
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Add City");
+                builder.setView(input);
+
+                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String newCity = input.getText().toString().trim();
+
+                        if (!newCity.isEmpty()) {
+                            dataList.add(newCity);
+                            cityAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", null);
+
+                builder.show();
+            }
+        });
+
+        Button deleteButton = findViewById(R.id.delete_button);
+
+        deleteButton.setOnClickListener(view -> {
+            if (selectedPosition != -1) {
+                dataList.remove(selectedPosition);
+                cityAdapter.notifyDataSetChanged();
+                selectedPosition = -1;
+            }
+        });
+
+
 
     }
 }
